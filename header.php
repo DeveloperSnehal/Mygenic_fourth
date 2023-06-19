e7b6ea2bca16a3be47ee3dc71907be4776ea7fa0
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-06-19 13:09:53 +0530

    changes in header section page

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <meta name="description" content="Provetta - Laboratory & Science Research HTML5 Template">
-  <link href="assets/images/favicon/favicon.png" rel="icon">
-  <title>Provetta - Laboratory & Science Research HTML5 Template</title>
+  <link href="assets/images/favicon/mygenic_favicon.png" rel="icon">
+  <title>MyGenic Healthcare</title>
 
   <link
     href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&amp;display=swap"
@@ -15,6 +15,8 @@
   <link rel="stylesheet" href="../../../use.fontawesome.com/releases/v5.15.1/css/all.css">
   <link rel="stylesheet" href="assets/css/libraries.css">
   <link rel="stylesheet" href="assets/css/style.css">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -33,39 +35,18 @@
             <div class="col-12">
               <div class="d-flex align-items-center justify-content-between">
                 <ul class="contact-list d-flex flex-wrap align-items-center list-unstyled mb-0">
-                  <li class="miniPopup-language-area">
-                    <button class="miniPopup-language-trigger" type="button">Change language</button>
-                    <ul class="miniPopup miniPopup-language list-unstyled">
-                      <li>
-                        <button>
-                          <img src="assets/images/flags/en.png" alt="en">
-                          <span>English</span>
-                        </button>
-                      </li>
-                      <li>
-                        <button>
-                          <img src="assets/images/flags/gr.png" alt="en">
-                          <span>Germany</span>
-                        </button>
-                      </li>
-                    </ul><!-- /.miniPopup-language -->
+                  <li>
+                    <i class="icon-location"></i><a href="#">Location:- Sector 35 Dolphin Platinum Kamothe, Navi Mumbai, Maharashtra, India</a>
                   </li>
                   <li>
-                    <p class="promo-text color-white d-flex align-items-center mb-0">
-                      <i class="icon-alert color-secondary mr-2"></i>
-                      <span>Express delivery and free returns within 24 hours </span>
-                    </p>
-                  </li>
-                </ul><!-- /.contact-list -->
-                <ul class="contact-list d-flex flex-wrap align-items-center list-unstyled mb-0">
-                  <li>
-                    <i class="icon-phone"></i><a href="tel:+5565454117">Emergency Line: (002) 01061245741</a>
+                    <i class="icon-phone"></i><a href="tel:+91 9867592446">Emergency Line:- +91 9867592446</a>
                   </li>
                   <li>
-                    <i class="icon-location"></i><a href="#">Location: Brooklyn, New York</a>
-                  </li>
-                  <li>
-                    <i class="icon-clock"></i><a href="contact-us.html">Mon - Fri: 8:00 am - 7:00 pm</a>
+                        <a href="#"><i class="icon-bi bi-facebook"></i></a>
+                        <a href="#"><i class="icon-bi bi-whatsapp"></i></a>
+                        <a href="#"><i class="icon-bi bi-instagram"></i></a>
+                        <a href="#"><i class="icon-bi bi-linkedin"></i></a>
+                        <a href="#"><i class="icon-bi bi-twitter"></i></a>
                   </li>
                 </ul><!-- /.contact-list -->
               </div>
@@ -76,7 +57,7 @@
       <nav class="navbar navbar-expand-lg sticky-navbar">
         <div class="container-fluid">
           <a class="navbar-brand" href="index.html">
-            <img src="assets/images/logo/logo-dark.png" class="logo-dark" alt="logo">
+            <img src="assets/images/logo/mygenic-logo-tm.png" class="logo-dark" alt="logo">
           </a>
           <button class="navbar-toggler" type="button">
             <span class="menu-lines"><span></span></span>
@@ -84,197 +65,36 @@
           <div class="collapse navbar-collapse justify-content-end" id="mainNavigation">
             <ul class="navbar-nav">
               <li class="nav-item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link active">Home</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item">
-                    <a href="index.html" class="nav-item-link">Home Main</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="home-modern.html" class="nav-item-link">Home Modern</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="home-classic.html" class="nav-item-link">Home Classic</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="home-supllies.html" class="nav-item-link">Home Supllies</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
+                <a href="index.php" class="nav-item-link active">Home</a>
+              </li><!-- /.nav-item -->
+              <li class="nav-item">
+                <a href="about.php" class="nav-item-link">About Us</a>
+              </li><!-- /.nav-item -->
+              <li class="nav-item">
+                <a href="#" class="nav-item-link">Our Products</a>
               </li><!-- /.nav-item -->
               <li class="nav-item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link">About Us</a>
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link">Services</a>
                 <ul class="dropdown-menu">
                   <li class="nav-item">
-                    <a href="about-us.html" class="nav-item-link">About Us</a>
+                    <a href="research-grid.html" class="nav-item-link">Contract Manufacturer</a>
                   </li><!-- /.nav-item -->
                   <li class="nav-item">
-                    <a href="team.html" class="nav-item-link">Leadership Team</a>
+                    <a href="research-modern.html" class="nav-item-link">Third Party Manufacturer</a>
                   </li><!-- /.nav-item -->
                   <li class="nav-item">
-                    <a href="why-us.html" class="nav-item-link">Why Choose Us</a>
+                    <a href="research-single.html" class="nav-item-link">Quality Medicine Supplier</a>
                   </li><!-- /.nav-item -->
                   <li class="nav-item">
-                    <a href="testimonials.html" class="nav-item-link">Testimonials</a>
+                    <a href="research-single.html" class="nav-item-link">Leading Pharmaceutical Exporter</a>
                   </li><!-- /.nav-item -->
                   <li class="nav-item">
-                    <a href="faqs.html" class="nav-item-link">Help & FAQs</a>
-                  </li> <!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="find-lab.html" class="nav-item-link">Find a Lab</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="supplies.html" class="nav-item-link">Supplies</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="careers.html" class="nav-item-link">Careers</a>
+                    <a href="research-single.html" class="nav-item-link">Research And Development</a>
                   </li><!-- /.nav-item -->
                 </ul><!-- /.dropdown-menu -->
               </li><!-- /.nav-item -->
-              <li class="nav-item has-dropdown has-mega-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link">Tests & Services</a>
-                <ul class="dropdown-menu mega-dropdown-menu">
-                  <li class="nav-item">
-                    <div class="container">
-                      <div class="row">
-                        <div class="col-sm-12 col-md-2 nav-block">
-                          <h2 class="nav-title">Tests & Services</h2>
-                          <ul class="nav flex-column">
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">General Diagnostic Tests</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Specialized Genetic Tests</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Food Sensitivity Tests</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Genova Diagnostics</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Hormone Insights Tests</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Naturopathic Tests</a>
-                            </li>
-                          </ul>
-                        </div><!-- /.col-md-2  -->
-                        <div class="col-sm-12 col-md-2 nav-block">
-                          <h2 class="nav-title">Healthcare Provider</h2>
-                          <ul class="nav flex-column">
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-provider-registration.html">Provider
-                                Registration</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Resources and Supplies</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-services.html">Tests and Services</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="faqs.html">Help and FAQs</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="provider-login.html">Provider Login</a>
-                            </li>
-                          </ul>
-                        </div><!-- /.col-md-2  -->
-                        <div class="col-sm-12 col-md-2 nav-block">
-                          <h2 class="nav-title">Patients</h2>
-                          <ul class="nav flex-column">
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-services.html">Tests and Services</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-prepare-test.html">Preparing for a Test</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-single-test.html">Check your Results</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-book-visit.html">Book a Lab Visit</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="faqs.html">Patients FAQs</a>
-                            </li>
-                          </ul>
-                        </div><!-- /.col-md-2  -->
-                        <div class="col-sm-12 col-md-2 nav-block">
-                          <h2 class="nav-title">Get Care</h2>
-                          <ul class="nav flex-column">
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-billing.html">Billing and Insurance</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="contact-us.html">Contact and Locations</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="tests-accreditation.html">Accreditation</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-item-link" href="testimonials.html">Testimonials</a>
-                            </li>
-                          </ul>
-                        </div><!-- /.col-md-2  -->
-                        <div class="col-sm-12 col-md-4 nav-block nav-block-featured bg-secondary">
-                          <div class="menu-shape"></div>
-                          <div>
-                            <h2 class="nav-title color-white">COVID19 Testing Solutions</h2>
-                            <p class="nav-desc color-white">Human coronaviruses cause infections of the nose, throat
-                              and
-                              lungs, they are most spread from an infected person.
-                            </p>
-                            <ul class="nav">
-                              <li class="nav-item">
-                                <a class="nav-item-link" href="#">
-                                  <i class="icon-arrow-right"></i><span>Workplaces</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-item-link" href="#">
-                                  <i class="icon-arrow-right"></i><span>Individuals</span>
-                                </a>
-                              </li>
-                              <li class="nav-item">
-                                <a class="nav-item-link" href="#">
-                                  <i class="icon-arrow-right"></i><span>Travellers</span>
-                                </a>
-                              </li>
-                            </ul>
-                            <a href="about-us.html" class="btn btn-primary mt-10">
-                              <span>Explore More</span> <i class="icon-arrow-right"></i>
-                            </a>
-                          </div>
-                        </div><!-- /.col-md-4  -->
-                      </div><!--  /.row  -->
-                    </div><!--  /.container  -->
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
-              </li><!-- /.nav-item -->
-              <li class="nav-item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link">Research</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item">
-                    <a href="research-grid.html" class="nav-item-link">Research Grid</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="research-modern.html" class="nav-item-link">Research Modern</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="research-single.html" class="nav-item-link">Single Research</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
-              </li><!-- /.nav-item -->
-              <li class="nav-item has-dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav-item-link">Blog</a>
-                <ul class="dropdown-menu">
-                  <li class="nav-item">
-                    <a href="blog.html" class="nav-item-link">Blog Grid</a>
-                  </li><!-- /.nav-item -->
-                  <li class="nav-item">
-                    <a href="blog-single-post.html" class="nav-item-link">Single Blog Post</a>
-                  </li><!-- /.nav-item -->
-                </ul><!-- /.dropdown-menu -->
+              <li class="nav-item">
+                <a href="#" class="nav-item-link">How We Do?</a>
               </li><!-- /.nav-item -->
               <li class="nav-item">
                 <a href="contact-us.html" class="nav-item-link">Contacts</a>
@@ -283,42 +103,9 @@
             </ul><!-- /.navbar-nav -->
             <ul class="header-actions d-flex align-items-center position-relative list-unstyled mb-0">
               <li>
-                <button class="action-btn-search"><i class="fa fa-search"></i></button>
-              </li>
-              <li>
                 <a href="tests-book-visit.html" class="btn btn-secondary btn-contact">
                   Book a Lab Visit
                 </a>
-              </li>
-              <li>
-                <a href="#" class="action-btn action-btn-cart">
-                  <i class="icon-cart"></i><span class="cart-counter">3</span>
-                </a>
-                <div class="cart-minipopup">
-                  <ul class="list-unstyled">
-                    <li class="cart-item">
-                      <div class="cart-img"><img src="assets/images/products/1.jpg" alt="thumb"></div>
-                      <div class="cart-content">
-                        <a class="cart-title" href="shop-single.html">Green Tea</a>
-                        <span class="cart-price">$ 4.00</span>
-                        <button class="cart-delete">&times;</button>
-                      </div><!-- /.cart-item-content -->
-                    </li><!-- /.cart-item -->
-                    <li class="cart-item">
-                      <div class="cart-img"><img src="assets/images/products/2.jpg" alt="thumb"></div>
-                      <div class="cart-content">
-                        <a class="cart-title" href="shop-single.html">Biotin Complex </a>
-                        <span class="cart-price">$ 26.00</span>
-                        <button class="cart-delete">&times;</button>
-                      </div><!-- /.cart-item-content -->
-                    </li><!-- /.cart-item -->
-                  </ul>
-                  <div class="cart-total">
-                    <span>Total:</span>
-                    <span>$14.00</span>
-                  </div><!-- /.cart-subtotal -->
-                  <a href="shopping-cart.html" class="btn btn-secondary btn-block">View Cart</a>
-                </div><!-- /.cart-minipopup -->
               </li>
             </ul>
             <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
